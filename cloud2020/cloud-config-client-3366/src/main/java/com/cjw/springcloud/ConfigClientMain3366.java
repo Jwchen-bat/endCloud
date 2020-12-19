ee0ae590e1ad756d43908635e1ed0a6c2cb35293
cjw <cjw807764069@126.com>
2020-12-20 00:59:38 +0800

    完成配置中心整合bus

---
+++ cloud2020/cloud-config-client-3366/src/main/java/com/cjw/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.cjw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 西楚霸王
+ * @date 2020/12/19 22:59
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
